0239c2ff4a3c7ea75f3338bb00d834d6e38a2c4d
Gerardo Miranda <gerardodev25@gmail.com>
2022-10-26 13:24:59 -0400

    Custom Types

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const gqlMiddleware = require('express-graphql');
-const { graphql, buildSchema } = require('graphql');
+const { buildSchema } = require('graphql');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const resolvers = require('./lib/resolvers.js');
 
 const app = express();
 const port = process.env.port || 3000;
 
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
+const schema = buildSchema(readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8'));
 
-// configurar
-const resolvers = {
-  hello: () => {
-    return 'hello world';
-  },
-};
+// // configurar
+// const resolvers = {
+//   hello: () => {
+//     return 'hello world';
+//   },
+// };
 
 app.use(
   '/api',
